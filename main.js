57d6c9d79d337536b087a921efaf138dcd00dd5e
Andrea Wu <lixianhyun@gmail.com>
2020-10-11 22:23:52 -0700

    display keys button, styling and adding effects to buttons, and add EvenListener to make functional buttons sounds

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 window.onload = function(){
 
 };
+
+window.addEventListener("keydown", function(e) {
+    
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stop the function from running all together
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add("buttonEffect");
+
+}); 
+
+window.addEventListener("keyup", function(e){
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.remove("buttonEffect");
+})
